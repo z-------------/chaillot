7384591bae68a0ae497d27f7075f0a9f0663f66f
Zack Guard <zacharyguard@gmail.com>
2017-01-23 20:23:44 +0800

    Stuff

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,33 @@
 // posting
 
 $(".post-form").on("submit", function(e) {
-    if (this.checkValidity()) {
-        e.preventDefault();
-    }
-
     var content = $(this).find(".post-form__text").val();
 
     jQuery.post("/api/post", {
         content: content
     }, function(data, status) {
         console.log("success", status);
+        fetchStream();
     });
+
+    $(this).find(".post-form__text").val("");
 });
 
 // fetch stream
 
-jQuery.ajax("/api/get_posts").done(function(posts) {
-    for (post of posts) {
-        var postElem = document.createElement("chaillot-post");
+function fetchStream() {
+    $(".stream").html("");
+    jQuery.ajax("/api/get_posts").done(function(posts) {
+        for (post of posts.reverse()) {
+            var postElem = document.createElement("chaillot-post");
 
-        postElem.authorName = post.author;
-        postElem.content = post.content;
-        postElem.date = new Date(post.date).toLocaleTimeString();
+            postElem.authorName = post.author;
+            postElem.content = post.content;
+            postElem.date = moment(post.date).calendar();
 
-        $(".stream").append(postElem);
-    }
-});
+            $(".stream").append(postElem);
+        }
+    });
+}
+
+fetchStream();
